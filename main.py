1f15b0437b1e98a35c1b7fc6fef8cc7d010c5e9a
Shibbu555 <118251808+Shibbu555@users.noreply.github.com>
2022-11-21 12:41:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-from flask import Flask,jsonify,request
-
+from flask import Flask, jsonify
 app = Flask(_name_)
 
+books_db=[
+     {
+        'name':'Secret',
+        'price':275
+    },
+    {
+        'name':'Doop Work',
+        'price':300
+    }
+]
+
+#retrive all the books
 @app.route('/')
 def home():
     return jsonify({'message':'welcome'})
@@ -14,8 +25,10 @@ def on():
 def off():
     return jsonify({'state':'0'})
 
+
+
 if __name__=='__main__':
-	app.run(debug=True)
+     app.run(debug=True)
 	
 
     
